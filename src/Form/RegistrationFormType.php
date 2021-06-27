f4d2e732f423eabff08092e41bdc8580f954303b
akia-web <59091758+akia-web@users.noreply.github.com>
2021-06-27 13:05:24 +0200

    css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -34,11 +34,10 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'attr' => array(
-                    'placeholder' => 'mot de passe'
-                ),
+              
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                            'placeholder' => 'mot de passe'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
